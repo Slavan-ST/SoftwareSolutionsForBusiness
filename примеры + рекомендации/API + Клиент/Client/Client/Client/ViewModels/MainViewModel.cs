acb47afbbe400e27cd092eca6ada078a14ad7b53
Slavan-ST <slavan990900000@gmail.com>
2024-03-29 22:25:24 +0700

    обновил всю документацию

--- примеры + рекомендации/API + Клиент/Client/Client/Client/ViewModels/MainViewModel.cs
+++ примеры + рекомендации/API + Клиент/Client/Client/Client/ViewModels/MainViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using API.Models;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Net;
 using System.Net.Cache;
 using System.Net.Http;
 using System.Net.Http.Json;
@@ -12,9 +13,10 @@ namespace Client.ViewModels
 
         public MainViewModel()
         {
-            Test();
+            TestAddStudent();
+            TestGetStudents();
         }
-        async void Test()
+        async void TestAddStudent()
         {
             HttpClient client = new HttpClient();
             var response = await client.PostAsJsonAsync("http://localhost:8080/Student", new Student()
@@ -22,9 +24,25 @@ namespace Client.ViewModels
                 Name = "new",
                 Surname = "new",
                 Patronymic = "new"
-            }) ;
+            });
 
             Debug.WriteLine(response);
+        }
+        async void TestGetStudents()
+        {
+            HttpClient client = new HttpClient();
+            var response = await client.GetFromJsonAsync<List<Student>>("http://localhost:8080/Student");
+
+            //при возврате null - закрываем void
+            if (response == null)
+            {
+                return;
+            }
+
+            foreach (var student in response)
+            {
+                Debug.WriteLine(student);
+            }
         }
     }
 }
